2ee52a93fa3aca34b48003dc27ac804eff7c3b95
Tamal2401 <das.tamal00496@gmail.com>
2020-09-27 23:51:39 +0530

    Form based authentication changes

---
+++ src/main/java/spring/security/jwt/security/auth/ApplicationUser.java
@@ -0,0 +1,67 @@
+package spring.security.jwt.security.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class ApplicationUser implements UserDetails {
+
+    public final Collection<? extends GrantedAuthority> authorities;
+    public final String password;
+    public final String username;
+    public final boolean isAccountNonExpired;
+    public final boolean isAccountNonLocked;
+    public final boolean isCredentialsNonExpired;
+    public final boolean isEnabled;
+
+    public ApplicationUser(Collection<? extends GrantedAuthority> authorities,
+                           String password, String username,
+                           boolean isAccountNonExpired,
+                           boolean isAccountNonLocked,
+                           boolean isCredentialsNonExpired,
+                           boolean isEnabled) {
+        this.authorities = authorities;
+        this.password = password;
+        this.username = username;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
